2e232aa16c092d315d2e44ef071c13279c12c3c6
Gisel <130872784+GiselCaicedo@users.noreply.github.com>
2025-09-08 18:14:58 -0500

    feat: add multi-instance qr web interface

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('port', PORT);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.use(whatsappRoutes);
 
 export default app;
